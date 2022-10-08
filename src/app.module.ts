53d697f80e82e26e2471fce0e54de6a643f82dc4
Carlos <carloszan@users.noreply.github.com>
2022-10-07 23:56:07 -0300

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { HealthModule } from './health/health.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, HealthModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiTags,
+  ApiProperty,
+  ApiBody,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+
+class LoginDto {
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  password: string;
+}
+
+@ApiTags('auth')
+@Controller()
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  @ApiOperation({ summary: 'Login user', description: 'forbidden' })
+  @ApiBody({ type: LoginDto })
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
