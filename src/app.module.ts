54a0dae5ab1efa431ddd57468e0e36b35f1cea62
Carlos <carloshlzansavio@gmail.com>
2022-10-20 01:33:09 -0300

    add mongoosedb

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { HealthModule } from './health/health.module';
 import { ExampleModule } from './example/example.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, UsersModule, HealthModule, ExampleModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    HealthModule,
+    ExampleModule,
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
   controllers: [],
   providers: [],
 })
