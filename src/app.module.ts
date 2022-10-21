e2a4e1b5794a2556e7c6ff631b30ac688ab27016
Carlos <carloshlzansavio@gmail.com>
2022-10-20 23:26:43 -0300

    delete example module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { HealthModule } from './health/health.module';
-import { ExampleModule } from './example/example.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
@@ -10,7 +9,6 @@ import { MongooseModule } from '@nestjs/mongoose';
     AuthModule,
     UsersModule,
     HealthModule,
-    ExampleModule,
     MongooseModule.forRoot(process.env.MONGO_URL),
   ],
   controllers: [],
--- src/example/example.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ExampleController } from './example.controller';
-
-@Module({
-  controllers: [ExampleController],
-  providers: [],
-})
-export class ExampleModule {}
