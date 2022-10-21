4314b41069e5c34c19e82414372288930ea1b0f2
Carlos <carloshlzansavio@gmail.com>
2022-10-20 23:55:14 -0300

    add tests for users.controller

---
+++ src/users/users.controller.spec.ts
@@ -0,0 +1,38 @@
+import { CreateUserDto } from './dtos/create.user.dto';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { Role } from './../enums/role.enum';
+
+describe('UserController', () => {
+  let controller: UsersController;
+  const createMockFn = jest.fn();
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            create: createMockFn,
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should create user', async () => {
+    const createUserDto: CreateUserDto = {
+      username: 'carlos',
+      password: 'changeme',
+      roles: [Role.Admin],
+    };
+    const result = await controller.create(createUserDto);
+
+    expect(result.username).toEqual('carlos');
+    expect(result.roles).toEqual([Role.Admin]);
+  });
+});
