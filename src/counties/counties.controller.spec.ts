a0122c801f6508840c0c68a538844abf76a18a15
Carlos <carloshlzansavio@gmail.com>
2022-10-25 17:09:25 -0300

    fix county module tests

--- src/counties/counties.controller.spec.ts
+++ src/counties/counties.controller.spec.ts
@@ -4,11 +4,14 @@ import { CountiesService } from './counties.service';
 
 describe('CountiesController', () => {
   let controller: CountiesController;
+  const createMockFn = jest.fn();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CountiesController],
-      providers: [CountiesService],
+      providers: [
+        { provide: CountiesService, useValue: { create: createMockFn } },
+      ],
     }).compile();
 
     controller = module.get<CountiesController>(CountiesController);
--- src/counties/counties.service.spec.ts
+++ src/counties/counties.service.spec.ts
@@ -1,12 +1,21 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CountiesService } from './counties.service';
+import { County } from './schemas/county.schema';
 
 describe('CountiesService', () => {
   let service: CountiesService;
+  const findOneMockFn = jest.fn();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CountiesService],
+      providers: [
+        CountiesService,
+        {
+          provide: getModelToken(County.name),
+          useValue: { findOne: findOneMockFn },
+        },
+      ],
     }).compile();
 
     service = module.get<CountiesService>(CountiesService);
