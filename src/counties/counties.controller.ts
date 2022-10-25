20b513c390dbc0f2c2bb8aa1a5b69963b5c99630
Carlos <carloshlzansavio@gmail.com>
2022-10-25 14:54:25 -0300

    add authentication and authorization

--- src/counties/counties.controller.ts
+++ src/counties/counties.controller.ts
@@ -7,11 +7,16 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CountiesService } from './counties.service';
 import { CreateCountyDto } from './dto/create-county.dto';
 import { UpdateCountyDto } from './dto/update-county.dto';
+import { JwtAuthGuard } from './../auth/jwt-auth.guard';
+import { RolesGuard } from './../guards/roles.guard';
+import { Roles } from './../decorators/roles.decorator';
+import { Role } from './../enums/role.enum';
 
 @ApiTags('counties')
 @Controller('counties')
@@ -21,6 +26,8 @@ export class CountiesController {
   @ApiOperation({ summary: 'Create new county', description: 'forbidden' })
   @ApiBody({ type: CreateCountyDto })
   @ApiResponse({ type: GetCountyDto })
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Cisab)
   @Post()
   create(@Body() createCountyDto: CreateCountyDto) {
     return this.countiesService.create(createCountyDto);
@@ -28,6 +35,8 @@ export class CountiesController {
 
   @ApiOperation({ summary: 'Find all counties', description: 'forbidden' })
   @ApiResponse({ type: GetCountyDto })
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Cisab)
   @Get()
   findAll() {
     return this.countiesService.findAll();
@@ -35,6 +44,8 @@ export class CountiesController {
 
   @ApiOperation({ summary: 'Find one county', description: 'forbidden' })
   @ApiResponse({ type: [GetCountyDto] })
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Cisab)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.countiesService.findOne(+id);
@@ -43,12 +54,16 @@ export class CountiesController {
   @ApiOperation({ summary: 'Update one county', description: 'forbidden' })
   @ApiBody({ type: UpdateCountyDto })
   @ApiResponse({ type: GetCountyDto })
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Cisab)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCountyDto: UpdateCountyDto) {
     return this.countiesService.update(+id, updateCountyDto);
   }
 
   @ApiOperation({ summary: 'Remove county', description: 'forbidden' })
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Cisab)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.countiesService.remove(+id);
