0bcfcb9581eaddc928d5d08127c6477bc65254da
Carlos <carloshlzansavio@gmail.com>
2022-11-16 20:29:32 -0300

    add upload product image

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,10 +6,18 @@ import {
   Patch,
   Param,
   Delete,
+  UploadedFile,
+  ParseFilePipe,
+  MaxFileSizeValidator,
+  UseInterceptors,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
 import { ProductsService } from './products.service';
 import { CreateProductRequest } from './dto/request/create-product-request.dto';
 import { UpdateProductRequest } from './dto/request/update-product-request.dto';
+
+const TWO_MBs = 2097152;
 
 @Controller('products')
 export class ProductsController {
@@ -45,5 +53,19 @@ export class ProductsController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
+  }
+
+  @UseInterceptors(FileInterceptor('file'))
+  @Post(':id/image')
+  uploadImage(
+    @Param('id') id: string,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new MaxFileSizeValidator({ maxSize: TWO_MBs })],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return `File size: ${file.size} bytes`;
   }
 }
