0d168f74440185e6f64a0031525a60151dbe7157
Carlos <carloshlzansavio@gmail.com>
2022-11-17 15:11:57 -0300

    add create product tests

--- src/products/products.controller.spec.ts
+++ src/products/products.controller.spec.ts
@@ -1,20 +1,52 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateProductRequest } from './dto/request/create-product-request.dto';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 describe('ProductsController', () => {
   let controller: ProductsController;
+  const createMockFn = jest.fn();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
-      providers: [{ provide: ProductsService, useValue: {} }],
+      providers: [
+        { provide: ProductsService, useValue: { create: createMockFn } },
+      ],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should creat', async () => {
+    const request: CreateProductRequest = {
+      name: 'string',
+      measurements: [
+        {
+          name: 'width',
+          value: '3',
+          unit: 'cm',
+        },
+      ],
+    };
+
+    const resService = {
+      _id: '635aece69b8ac8a5056875a2',
+      name: 'string',
+      measurements: [
+        {
+          name: 'width',
+          value: '3',
+          unit: 'cm',
+        },
+      ],
+    };
+    createMockFn.mockReturnValue(Promise.resolve(resService));
+
+    const res = await controller.create(request);
+
+    expect(res._id).toEqual('635aece69b8ac8a5056875a2');
+    expect(res.name).toEqual('string');
+    expect(res.measurements.length).toEqual(1);
   });
 });
--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -1,24 +1,123 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UnitsService } from '../units/units.service';
+import { CreateProductRequest } from './dto/request/create-product-request.dto';
 import { ProductsRepository } from './products.repository';
 import { ProductsService } from './products.service';
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  const findOneMockFn = jest.fn();
+  const createMockFn = jest.fn();
+
+  const startTransactionMockFn = jest.fn();
+  startTransactionMockFn.mockReturnValue(
+    Promise.resolve({
+      abortTransaction: jest.fn(),
+      commitTransaction: jest.fn(),
+    }),
+  );
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         ProductsService,
-        { provide: ProductsRepository, useValue: {} },
-        { provide: UnitsService, useValue: {} },
+        {
+          provide: ProductsRepository,
+          useValue: {
+            create: createMockFn,
+            startTransaction: startTransactionMockFn,
+          },
+        },
+        { provide: UnitsService, useValue: { findOne: findOneMockFn } },
       ],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should create product', async () => {
+    const request: CreateProductRequest = {
+      name: 'string',
+      measurements: [
+        {
+          name: 'width',
+          value: '3',
+          unit: 'cm',
+        },
+      ],
+    };
+
+    findOneMockFn.mockReturnValue(Promise.resolve());
+
+    const productRes = {
+      _id: '635aece69b8ac8a5056875a2',
+      name: 'string',
+      measurements: [
+        {
+          name: 'width',
+          value: '3',
+          unit: 'cm',
+        },
+      ],
+    };
+
+    createMockFn.mockReturnValue(Promise.resolve(productRes));
+
+    const product = await service.create(request);
+
+    expect(product._id).toEqual('635aece69b8ac8a5056875a2');
+    expect(product.name).toEqual('string');
+    expect(product.measurements.length).toEqual(1);
+  });
+
+  it('should abort if create breaks', async () => {
+    const request: CreateProductRequest = {
+      name: 'string',
+      measurements: [
+        {
+          name: 'width',
+          value: '3',
+          unit: 'cm',
+        },
+      ],
+    };
+
+    findOneMockFn.mockReturnValue(Promise.resolve());
+
+    createMockFn.mockImplementation(() => {
+      throw new Error();
+    });
+
+    try {
+      await service.create(request);
+      expect(false).toBeTruthy();
+    } catch (err) {
+      expect(err).toBeInstanceOf(Error);
+    }
+  });
+
+  it('should throw a bad request if unit doesnt exist', async () => {
+    const request: CreateProductRequest = {
+      name: 'string',
+      measurements: [
+        {
+          name: 'width',
+          value: '3',
+          unit: 'cm',
+        },
+      ],
+    };
+
+    findOneMockFn.mockImplementation(() => {
+      throw new Error();
+    });
+
+    try {
+      await service.create(request);
+      expect(false).toBeTruthy();
+    } catch (err) {
+      expect(err).toBeInstanceOf(BadRequestException);
+    }
   });
 });
