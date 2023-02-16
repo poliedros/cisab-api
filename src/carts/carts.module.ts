6c831b124d9eebb0866f8f835d6dd55c0d194bc0
Carlos <carloshlzansavio@gmail.com>
2023-02-16 13:08:31 -0300

    add carts controller

--- src/carts/carts.module.ts
+++ src/carts/carts.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Cart, CartSchema } from './schemas/cart.schema';
+import { CartsController } from './carts.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Cart.name, schema: CartSchema }]),
   ],
+  controllers: [CartsController],
 })
 export class CartsModule {}
