12a6fa76905e2100857153118a64e6e204660b9b
Carlos <carloshlzansavio@gmail.com>
2023-02-23 10:16:28 -0300

    Add CartsService and related dependencies to CartsController and CartsService specs.

--- src/carts/carts.controller.spec.ts
+++ src/carts/carts.controller.spec.ts
@@ -1,18 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CartsController } from './carts.controller';
+import { CartsService } from './carts.service';
 
 describe('CartsController', () => {
   let controller: CartsController;
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     controllers: [CartsController],
-  //   }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CartsController],
+      providers: [
+        {
+          provide: CartsService,
+          useValue: {},
+        },
+      ],
+    }).compile();
 
-  //   controller = module.get<CartsController>(CartsController);
-  // });
+    controller = module.get<CartsController>(CartsController);
+  });
 
   it('should be defined', () => {
-    expect(true).toBeTruthy();
+    expect(controller).toBeDefined();
   });
 });
--- src/carts/carts.service.spec.ts
+++ src/carts/carts.service.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CountiesService } from '../counties/counties.service';
+import { DemandsService } from '../demands/demands.service';
+import { UsersService } from '../users/users.service';
+import { ProductsService } from '../products/products.service';
+import { CartsCacheRepository } from './carts.cache.repository';
+import { CartsMongoRepository } from './carts.mongo.repository';
 import { CartsService } from './carts.service';
 
 describe('CartsService', () => {
   let service: CartsService;
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     providers: [CartsService],
-  //   }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CartsService,
+        {
+          provide: CartsCacheRepository,
+          useValue: {},
+        },
+        {
+          provide: CartsMongoRepository,
+          useValue: {},
+        },
+        {
+          provide: ProductsService,
+          useValue: {},
+        },
+        {
+          provide: DemandsService,
+          useValue: {},
+        },
+        {
+          provide: CountiesService,
+          useValue: {},
+        },
+        {
+          provide: UsersService,
+          useValue: {},
+        },
+      ],
+    }).compile();
 
-  //   service = module.get<CartsService>(CartsService);
-  // });
+    service = module.get<CartsService>(CartsService);
+  });
 
   it('should be defined', () => {
-    expect(true).toBeTruthy();
+    expect(service).toBeDefined();
   });
 });
